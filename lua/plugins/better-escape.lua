cece812686d55c444a853656af5575fe29d89d9e
Élie Caratgé <elie@bsport.io>
2024-09-08 19:42:42 +0200

    feat: add better-escape plugin

---
+++ lua/plugins/better-escape.lua
@@ -0,0 +1,37 @@
+return {
+  {
+    "max397574/better-escape.nvim",
+    config = function()
+      require("better_escape").setup({
+        default_mappings = false,
+        mappings = {
+          i = {
+            j = {
+              k = "<Esc>",
+              j = "<Esc>",
+            },
+          },
+          c = {
+            j = {
+              k = "<Esc>",
+              j = "<Esc>",
+            },
+          },
+          v = {
+            j = {
+              k = "<Esc>",
+            },
+          },
+          s = {
+            j = {
+              k = "<Esc>",
+            },
+          },
+          t = {
+            j = { k = false, j = false },
+          },
+        },
+      })
+    end,
+  },
+}
