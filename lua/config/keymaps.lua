179961b4b2754b2917105033cd89650934a11dcb
Élie Caratgé <elie@bsport.io>
2024-09-08 19:42:59 +0200

    feat: full screen option for neovide

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+function Neovide_fullscreen()
+  if vim.g.neovide_fullscreen == true then
+    vim.g.neovide_fullscreen = false
+  else
+    vim.g.neovide_fullscreen = true
+  end
+end
+
+vim.api.nvim_set_keymap("n", "<F11>", ":lua Neovide_fullscreen()<CR>", { noremap = true, silent = true })
