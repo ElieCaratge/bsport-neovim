9f596ec5d2f2cd98e73a66432f1b74684e1e116f
Élie Caratgé <elie@bsport.io>
2024-09-08 19:42:27 +0200

    feat: share clipboard with system

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Use system clipboard with "+" register
+vim.opt.clipboard = "unnamedplus"
+
 -- Enable the option to require a Prettier config file
 -- If no prettier config file is found, the formatter will not be used
 vim.g.lazyvim_prettier_needs_config = false
